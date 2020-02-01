b2084acfbf0bb6308a273c328a66b12625fe4424
LiuPan <1573802540@qq.com>
2020-02-01 20:10:28 +0800

    --

---
+++ MyTestServer/AppServer.cs
@@ -0,0 +1,23 @@
+﻿using Photon.SocketServer;
+
+namespace MyTestServer
+{
+    public class AppServer : ApplicationBase
+    {
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            var firstPeer = new FirstPeer(initRequest);
+            return firstPeer;
+        }
+
+        protected override void Setup()
+        {
+          
+        }
+
+        protected override void TearDown()
+        {
+           
+        }
+    }
+}
---
+++ MyTestServer/FirstPeer.cs
@@ -0,0 +1,23 @@
+﻿using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace MyTestServer
+{
+    public class FirstPeer : ClientPeer
+    {
+        public FirstPeer(InitRequest initRequest) : base(initRequest)
+        {
+            
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+    
+        }
+    }
+}
