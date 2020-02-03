a038d46084daa77ccd0ee59c0e6c2ccbeba0ea9a
L <unkown@.com>
2020-02-03 11:41:41 +0800

    服务器发送事件示例

--- MyTestServer/FirstPeer.cs
+++ MyTestServer/FirstPeer.cs
@@ -17,9 +17,15 @@ namespace MyTestServer
             var obj = request.Parameters[3];
             LogUtil.I("接收到客户端的请求：" + obj + "  OperationCode:" + request.OperationCode);
 
-            var dictionary = new Dictionary<byte, Object> {{1, "我是来自服务器的消息 i am from server"}};
+            var dictionary = new Dictionary<byte, Object> {{1, "我是来自服务器的响应 i am response msg from server"}};
+            //以响应的发送给客服端发送消息
             var response = new OperationResponse(1, dictionary);
             SendOperationResponse(response, sendParameters);
+
+            //以事件的方式给客服端发送消息
+            var dic2 = new Dictionary<byte, Object> {{1, "我是来自服务器的事件 i am event msg from server"}};
+            var eventData = new EventData(1, dic2);
+            SendEvent(eventData, sendParameters);
             LogUtil.I("===========服务端发送消息成功===========");
         }
 
